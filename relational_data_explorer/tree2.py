0d9c968adb8d3bea3666368b3d1aaa094f69935a
James Clevenger <jeclevenger@caslmedia.com>
2015-06-15 17:48:37 -0500

    relational data explorler filenames tree recursive

---
+++ relational_data_explorer/tree2.py
@@ -0,0 +1,19 @@
+
+import os
+import json
+
+walk_dir = os.path.join("sample_dir")
+
+def walk_path(path):
+    head, tail = os.path.split(path)
+    path_dict = {'name': tail}
+    if os.path.isdir(path):
+        path_dict['children'] = []
+        for child_path in os.listdir(path):
+            if child_path.startswith('.'):
+                continue
+            path_dict['children'].append(walk_path(path + '/' + child_path))
+    return path_dict
+
+with open('structure_output.json', 'w') as outfile:
+    json.dump(walk_path(walk_dir), outfile, indent=4)
